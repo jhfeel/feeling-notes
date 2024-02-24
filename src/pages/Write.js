3794e725a24f1590e1833334365b712a32a45d22
Jinhong Kim <127109684+jinhongcode@users.noreply.github.com>
2024-02-24 11:45:51 +0900

    feat: 감정 리스트 추가 및 감정 버튼 표시

--- src/pages/Write.js
+++ src/pages/Write.js
@@ -1,3 +1,4 @@
+import EmotionButton from "../components/EmotionButton";
 import Header from "../components/Header";
 import MenuBar from "../components/MenuBar";
 import MobileBottomBar from "../components/MobileBottomBar";
@@ -5,6 +6,42 @@ import SideBar from "../components/SideBar";
 import { formatDateTime } from "../utils/dateTime";
 
 const Write = () => {
+  const emotionList = [
+    "감사 🙏",
+    "만족 😌",
+    "희망 🌟",
+    "행복 😊",
+    "기쁨 😁",
+    "쾌감 😁",
+    "신남 🤩",
+    "열망 😍",
+    "자랑스러움 😎",
+    "성취감 🏆",
+    "다정함 🥰",
+    "기대감 😃",
+    "사랑 💕",
+    "우울 😞",
+    "불안 😟",
+    "걱정 😦",
+    "화남 😠",
+    "자신 없음 🙁",
+    "절망적 😩",
+    "부담감 😫",
+    "창피함 😳",
+    "속상함 😢",
+    "슬픔 😭",
+    "실망 😕",
+    "미안함 😓",
+    "귀찮음 😒",
+    "후회 😔",
+  ];
+
+  const emotionButtonList = [];
+
+  emotionList.forEach((emotion) => {
+    emotionButtonList.push(<EmotionButton emotionName={emotion} />);
+  });
+
   return (
     <div className="container">
       <MenuBar />
@@ -20,7 +57,7 @@ const Write = () => {
           <h4>어떤 상황이었나요?</h4>
           <textarea />
           <h4>그때, 어떤 감정을 느꼈나요?</h4>
-          <textarea />
+          <div className="emotionButtonList-container">{emotionButtonList}</div>
           <h4>그 순간, 어떤 생각을 하고 있었나요?</h4>
           <textarea />
           <h4>메모하기</h4>
