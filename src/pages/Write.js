d74ce2d431cbac9cca5b2fb10ec31c66dfc3b282
Jinhong Kim <127109684+jinhongcode@users.noreply.github.com>
2024-02-26 09:40:37 +0900

    feat: 작성 페이지 일시, 상황, 생각, 메모 state 추가

--- src/pages/Write.js
+++ src/pages/Write.js
@@ -4,9 +4,15 @@ import Header from "../components/Header";
 import MenuBar from "../components/MenuBar";
 import MobileBottomBar from "../components/MobileBottomBar";
 import SideBar from "../components/SideBar";
+import { formatDateTime } from "../utils/dateTime";
 
 const Write = () => {
+  const [dateTime, setDateTime] = useState(formatDateTime(new Date()));
+  const [situation, setSituation] = useState("");
   const [selectedEmotionList, setSelectedEmotionList] = useState([]);
+  const [thoughts, setThoughts] = useState("");
+  const [memo, setMemo] = useState("");
+
   const emotionList = [
     "감사 🙏",
     "만족 😌",
@@ -49,7 +55,6 @@ const Write = () => {
     );
   });
 
-  console.log(selectedEmotionList);
   return (
     <div className="container">
       <MenuBar />
@@ -57,15 +62,37 @@ const Write = () => {
         <Header />
         <div className="contents-container">
           <h4>언제인가요?</h4>
-          <input type="datetime-local" name="dateTime"></input>
+          <input
+            type="datetime-local"
+            name="dateTime"
+            value={dateTime}
+            onChange={(e) => {
+              setDateTime(e.target.value);
+            }}
+          ></input>
           <h4>어떤 상황이었나요?</h4>
-          <textarea />
+          <textarea
+            value={situation}
+            onChange={(e) => {
+              setSituation(e.target.value);
+            }}
+          />
           <h4>그때, 어떤 감정을 느꼈나요?</h4>
           <div className="emotionButtonList-container">{emotionButtonList}</div>
           <h4>그 순간, 어떤 생각을 하고 있었나요?</h4>
-          <textarea />
+          <textarea
+            value={thoughts}
+            onChange={(e) => {
+              setThoughts(e.target.value);
+            }}
+          />
           <h4>메모하기</h4>
-          <textarea />
+          <textarea
+            value={memo}
+            onChange={(e) => {
+              setMemo(e.target.value);
+            }}
+          />
         </div>
         <MobileBottomBar />
       </div>
