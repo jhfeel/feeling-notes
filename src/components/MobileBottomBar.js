6b2e5f6383eb492d178a2555a7a257a1f346ae82
Jinhong Kim <127109684+jinhongcode@users.noreply.github.com>
2024-03-03 09:10:14 +0900

    feat: 바텀바 라우팅 기능

--- src/components/MobileBottomBar.js
+++ src/components/MobileBottomBar.js
@@ -1,13 +1,27 @@
+import { useNavigate } from "react-router-dom";
+
 const MobileBottomBar = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="MobileBottomBar">
       <div className="bottom-item">
-        <div className="write-icon"></div>
+        <div
+          className="write-icon clickable"
+          onClick={() => {
+            navigate("/write");
+          }}
+        ></div>
       </div>
       <div className="bottom-item">
-        <div className="home-icon"></div>
+        <div
+          className="home-icon clickable"
+          onClick={() => {
+            navigate("/");
+          }}
+        ></div>
       </div>
-      <div className="bottom-item">
+      <div className="bottom-item clickable">
         <div className="profile-icon"></div>
       </div>
     </div>
