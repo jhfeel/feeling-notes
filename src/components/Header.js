bf9cb3d35601fa15f1cf094ad9581eb31bd98d8a
Jinhong Kim <127109684+jinhongcode@users.noreply.github.com>
2024-03-08 18:05:00 +0900

    feat: 헤더 타이틀 클릭 시 홈으로 이동 라우팅 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,19 @@
+import { useNavigate } from "react-router-dom";
+
 const Header = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="Header">
       <div className="header-left-icon"></div>
-      <div className="header-title">{"Feeling Notes"}</div>
+      <div
+        className="header-title clickable"
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        {"Feeling Notes"}
+      </div>
       <div className="header-right-icon"></div>
     </div>
   );
