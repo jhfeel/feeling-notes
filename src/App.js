0f75de27263e98844fef4b80fdad4befcd0019b5
Jinhong Kim <127109684+jinhongcode@users.noreply.github.com>
2024-03-16 10:54:14 +0900

    feat: Vercel 서버리스 함수로 DB 데이터 가져오기

--- src/App.js
+++ src/App.js
@@ -5,72 +5,63 @@ import Edit from "./pages/Edit";
 import Detail from "./pages/Detail";
 import { useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { createClient } from "@supabase/supabase-js";
-
-const supabase = createClient(
-  process.env.REACT_APP_SUPABASE_URL,
-  process.env.REACT_APP_SUPABASE_KEY
-);
 
 function App() {
   const [userNotes, setUserNotes] = useState([]);
 
-  const getUserNotes = async () => {
-    const { data } = await supabase.from("notes").select();
-    setUserNotes(data);
-  };
-
   useEffect(() => {
-    getUserNotes();
+    fetch("/api/get")
+      .then((response) => response.json())
+      .then((result) => {
+        setUserNotes(result);
+      });
   }, []);
 
   const onCreate = async (newNote) => {
-    const { data, error } = await supabase
-      .from("notes")
-      .insert({ ...newNote })
-      .select();
-    if (error) {
-      console.log("삽입 에러", error);
-    } else {
-      console.log("삽입 성공", data);
-      setUserNotes([...userNotes, ...data]);
-    }
+    // const { data, error } = await supabase
+    //   .from("notes")
+    //   .insert({ ...newNote })
+    //   .select();
+    // if (error) {
+    //   console.log("삽입 에러", error);
+    // } else {
+    //   console.log("삽입 성공", data);
+    //   setUserNotes([...userNotes, ...data]);
+    // }
   };
 
   const onEdit = async (targetNoteId, EditedNote) => {
-    const { data, error } = await supabase
-      .from("notes")
-      .update({ ...EditedNote })
-      .eq("id", targetNoteId)
-      .select();
-
-    if (error) {
-      console.log("수정 에러", error);
-    } else {
-      console.log("수정 성공", data);
-      setUserNotes(
-        userNotes.map((note) =>
-          note.id === targetNoteId ? { id: targetNoteId, ...EditedNote } : note
-        )
-      );
-    }
+    // const { data, error } = await supabase
+    //   .from("notes")
+    //   .update({ ...EditedNote })
+    //   .eq("id", targetNoteId)
+    //   .select();
+    // if (error) {
+    //   console.log("수정 에러", error);
+    // } else {
+    //   console.log("수정 성공", data);
+    //   setUserNotes(
+    //     userNotes.map((note) =>
+    //       note.id === targetNoteId ? { id: targetNoteId, ...EditedNote } : note
+    //     )
+    //   );
+    // }
   };
 
   const onRemove = async (targetNoteId) => {
-    const { error } = await supabase
-      .from("notes")
-      .delete()
-      .eq("id", targetNoteId);
-
-    if (error) {
-      console.log("삭제 에러", error);
-    } else {
-      console.log("삭제 성공");
-      setUserNotes(
-        userNotes.filter((note) => note.id !== Number(targetNoteId))
-      );
-      console.log(userNotes);
-    }
+    // const { error } = await supabase
+    //   .from("notes")
+    //   .delete()
+    //   .eq("id", targetNoteId);
+    // if (error) {
+    //   console.log("삭제 에러", error);
+    // } else {
+    //   console.log("삭제 성공");
+    //   setUserNotes(
+    //     userNotes.filter((note) => note.id !== Number(targetNoteId))
+    //   );
+    //   console.log(userNotes);
+    // }
   };
 
   return (
